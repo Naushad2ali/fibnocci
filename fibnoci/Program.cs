52be3d438b3ed6709943b458e8a18a4a324635ea
Naushad ali <99749562+Naushad1ali@users.noreply.github.com>
2022-03-04 00:03:05 +0530

    Add project files.

---
+++ fibnoci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibnoci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int n2 = 1;
+            int next;
+            for(int i = 0; i < 10; i++)
+            {
+                Console.Write(n);
+                next = n + n2;
+                n = n2;
+                n2 = next;  
+                   
+            }
+
+        }
+    }
+}
